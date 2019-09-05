97726a4baf70c897a85bd77079fe1e7da9816e48
mikong <4162+mikong@users.noreply.github.com>
2019-09-05 22:44:31 +0800

    feat: Track restaurant taggings on add/remove of menu items

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -6,4 +6,24 @@ class MenuItem < ApplicationRecord
 
   monetize :price_cents
 
+  after_create :increment_tagging
+  after_destroy :decrement_tagging
+
+  private
+
+    def increment_tagging
+      tagging = Tagging.find_or_create_by(restaurant: self.restaurant, tag: self.tag)
+      tagging.increment!(:taggings_count, 1)
+    end
+
+    def decrement_tagging
+      tagging = Tagging.find_by(restaurant: self.restaurant, tag: self.tag)
+      if tagging.present?
+        if tagging.taggings_count > 1
+          tagging.decrement!(:taggings_count, 1)
+        else
+          tagging.destroy
+        end
+      end
+    end
 end
