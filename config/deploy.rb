830e154d729e56b5ced5ea64e270dc62c3ead236
mikong <4162+mikong@users.noreply.github.com>
2019-09-07 21:23:55 +0800

    chore: Add initial mina deploy config

---
+++ config/deploy.rb
@@ -0,0 +1,73 @@
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+require 'mina/puma'
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :application_name, 'meal-delivery'
+set :domain, 'meal-delivery'
+set :deploy_to, '/var/www/meal-delivery'
+set :repository, 'https://github.com/mikong/rails-meal-delivery.git'
+set :branch, 'master'
+
+# Optional settings:
+#   set :user, 'foobar'          # Username in the server to SSH to.
+#   set :port, '30000'           # SSH port number.
+#   set :forward_agent, true     # SSH forward_agent.
+
+set :user, 'mikong'
+
+# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
+# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
+# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+set :shared_dirs, fetch(:shared_dirs, []).push('tmp/pids', 'tmp/sockets')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/master.key')
+
+# This task is the environment that is loaded for all remote run commands, such as
+# `mina deploy` or `mina rake`.
+task :remote_environment do
+  invoke :'rbenv:load'
+end
+
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  command %{touch "#{fetch(:shared_path)}/config/database.yml"}
+end
+
+# desc "First deploy only: git clone, shared paths, bundle install"
+# task :first_deploy do
+#   deploy do
+#     invoke :'git:clone'
+#     invoke :'deploy:link_shared_paths'
+#     invoke :'bundle:install'
+#     invoke :'rails:db_create'
+#     invoke :'rails:db_schema_load'
+#   end
+# end
+
+desc "Deploys the current version to the server."
+task :deploy do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    on :launch do
+      in_path(fetch(:current_path)) do
+        invoke :'puma:phased_restart'
+      end
+    end
+  end
+end
