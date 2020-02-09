49e306aacf709f55a0406b81036387c00e1c9dcd
mikong <4162+mikong@users.noreply.github.com>
2020-02-10 00:16:14 +0800

    test: Add initial MenuItem tests

--- test/factories.rb
+++ test/factories.rb
@@ -4,4 +4,9 @@ FactoryBot.define do
   factory :restaurant do
     name { Faker::Restaurant.name }
   end
+
+  factory :menu_item do
+    name { Faker::Food.dish }
+    price { rand(2..5) }
+  end
 end
--- test/models/menu_item_test.rb
+++ test/models/menu_item_test.rb
@@ -3,7 +3,30 @@
 require 'test_helper'
 
 class MenuItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save valid menu item' do
+    restaurant = create(:restaurant)
+    menu_item = MenuItem.new(
+      name: 'Bulalo',
+      price: 20,
+      tag: tags(:meat),
+      restaurant: restaurant
+    )
+    menu_item.save
+    assert menu_item.persisted?
+  end
+
+  test 'should increment tagging count' do
+    restaurant = create(:restaurant)
+    menu_item = create(:menu_item,
+      restaurant: restaurant,
+      tag: tags(:meat))
+    tagging = menu_item.find_restaurant_tagging
+    assert_equal 1, tagging.taggings_count
+
+    create(:menu_item,
+      restaurant: restaurant,
+      tag: tags(:meat))
+    tagging.reload
+    assert_equal 2, tagging.taggings_count
+  end
 end
