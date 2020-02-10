84585908930dd2d6de8f36f4823c3c845987b8b1
mikong <4162+mikong@users.noreply.github.com>
2020-02-10 14:22:31 +0800

    test: Test MenuItem decrements tagging count

--- test/models/menu_item_test.rb
+++ test/models/menu_item_test.rb
@@ -3,30 +3,53 @@
 require 'test_helper'
 
 class MenuItemTest < ActiveSupport::TestCase
+  setup do
+    @restaurant = create(:restaurant)
+  end
+
   test 'should save valid menu item' do
-    restaurant = create(:restaurant)
     menu_item = MenuItem.new(
       name: 'Bulalo',
       price: 20,
       tag: tags(:meat),
-      restaurant: restaurant
+      restaurant: @restaurant
     )
     menu_item.save
     assert menu_item.persisted?
   end
 
   test 'should increment tagging count' do
-    restaurant = create(:restaurant)
     menu_item = create(:menu_item,
-      restaurant: restaurant,
+      restaurant: @restaurant,
       tag: tags(:meat))
     tagging = menu_item.find_restaurant_tagging
     assert_equal 1, tagging.taggings_count
 
     create(:menu_item,
-      restaurant: restaurant,
+      restaurant: @restaurant,
       tag: tags(:meat))
     tagging.reload
     assert_equal 2, tagging.taggings_count
+  end
+
+  test 'should decrement tagging count' do
+    menu_items = create_list(:menu_item, 3,
+      restaurant: @restaurant,
+      tag: tags(:chicken))
+    tagging = menu_items.first.find_restaurant_tagging
+    assert_equal 3, tagging.taggings_count
+
+    menu_items.pop.destroy
+    tagging.reload
+    assert_equal 2, tagging.taggings_count
+
+    menu_items.pop.destroy
+    tagging.reload
+    assert_equal 1, tagging.taggings_count
+
+    menu_items.pop.destroy
+    assert_raises(ActiveRecord::RecordNotFound) do
+      tagging.reload
+    end
   end
 end
